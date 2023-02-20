bf460e466077fefb7df14e4f40f1cf2b0569bb30
Rasheed Mohammad <rasheed.m@live.com>
2023-02-20 23:06:34 +0530

    first commit

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,39 @@
+let js = 'amazing';
+console.log(30+9-1+5-12);
+
+let fName = 'bob';
+console.log(fName);
+
+let _name = "david";
+let $name = "richard";
+
+let name = "richard";
+console.log(name);
+
+// let new = "richard";
+
+
+// typeof operator: 
+console.log(typeof true);
+
+// Variable reassignment: 
+let data = true;
+console.log(typeof data); //boolean
+data = "david";
+console.log(typeof data); //string
+
+
+let year;
+console.log(year); // undefined - value
+console.log(typeof year);  //undefined - type of value
+
+console.log(typeof null);  //null
+
+
+
+
+
+
+
+
+
---
+++ assignment/assignement.js
@@ -0,0 +1,6 @@
+let country = "India";
+let continent = "Asia";
+let population = "1.4 billion";
+console.log(country);
+console.log(continent);
+console.log(population);
